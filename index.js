688fd4e3fdca0d73cc946aac9e037eee291f2196
adamwojnicki <wojnicki.adam1@gmail.com>
2025-03-29 22:31:36 +0100

    ddown

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ class Dropdown {
     }
 
     toggle() {
-        this.isOpen = !this.isOpen;
+        // this.isOpen = !this.isOpen;
         this.dropdown.classList.toggle('is-active', this.isOpen);
         this.button.setAttribute('aria-expanded', this.isOpen);
         this.list.setAttribute('aria-hidden', !this.isOpen);
@@ -17,29 +17,17 @@ class Dropdown {
 
     init() {
         this.button.addEventListener('click', () => {
+            this.isOpen = !this.isOpen;
             this.toggle();
         });
 
-        this.button.addEventListener('keydown', (event) => {
-            if (event.key === 'Enter') {
-                // this.toggle();
-            }
-            if (event.key === 'Escape') {
-                this.toggle();
-            }
-        });
-
-        // document.addEventListener('click', (event) => {
-        //     if (!this.dropdown.contains(event.target)) {
-        //         this.toggle();
-        //     }
-        // });
-
         this.list.addEventListener('keydown', (event) => {
             if (event.key === 'Escape') {
+                this.isOpen = false;
                 this.toggle();
             }
             if (event.key === 'Enter') {
+                this.isOpen = false;
                 this.toggle();
             }
         });
