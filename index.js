553fb2811846b48d2449e8e25e1cf1857d3f155a
adamwojnicki <wojnicki.adam1@gmail.com>
2025-03-29 22:54:17 +0100

    impreved keyboard nav + initial validation

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 class Dropdown {
-    constructor(dropdown, ddBtnSelector, ddListSelector) {
+    constructor(dropdown, ddBtnSelector, ddListSelector, ddListItemSelector) {
         this.dropdown = dropdown;
-        this.button = dropdown.querySelector(ddBtnSelector) || dropdown.querySelector('.js-dropdown-button');
-        this.list = dropdown.querySelector(ddListSelector) || dropdown.querySelector('.js-dropdown-list');
+        this.button = dropdown.querySelector(ddBtnSelector) || dropdown.querySelector('[role="comboBox"]');
+        this.list = dropdown.querySelector(ddListSelector) || dropdown.querySelector('[role="menu"]');
+        this.listItems = dropdown.querySelectorAll(ddListItemSelector) || dropdown.querySelectorAll('[role="menuitem"]');
         this.isOpen = false;
 
+        if (!this.button || !this.list || !this.listItems) {
+            console.error('Dropdown initialization failed. Missing required elements.');
+            return;
+        }
         this.init();
     }
 
     toggle() {
-        // this.isOpen = !this.isOpen;
         this.dropdown.classList.toggle('is-active', this.isOpen);
         this.button.setAttribute('aria-expanded', this.isOpen);
         this.list.setAttribute('aria-hidden', !this.isOpen);
@@ -19,6 +23,14 @@ class Dropdown {
         this.button.addEventListener('click', () => {
             this.isOpen = !this.isOpen;
             this.toggle();
+            this.list.querySelector('.dropdown__item')?.focus();
+        });
+
+        this.button.addEventListener('keydown', (event) => {
+            if (event.key === 'Escape') {
+                this.isOpen = false;
+                this.toggle();
+            }
         });
 
         this.list.addEventListener('keydown', (event) => {
